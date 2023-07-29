f0f9a360fc5c4f9675ea2f1528cf086783c45249
juliocms <juliocms@gmail.com>
2023-07-29 15:45:20 -0300

    Adicionada a pasta output de tests no ignore do git; Adicionado o bash para executar dentro do container

---
+++ scripts/run_event_dispatcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Permissão de execução ao script event_dispatcher.py
+chmod +x /app/src/event_dispatcher.py
+
+# Navega para o diretório /app/src onde está o arquivo event_dispatcher.py
+cd /app/src
+
+# Comando para executar o event_dispatcher.py
+python event_dispatcher.py
